6e8bc7c0dd317dfb20a43c4aa234a0decf48a377
Tish <latisha.griffiths@programmer.net>
2019-04-22 02:23:54 -0500

    added a callback funcion to mongoose.connect()

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -6,7 +6,10 @@ const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb://dbUser:${process.env.dbUserPassword}@list-app-shard-00-00-c9msa.mongodb.net:27017,list-app-shard-00-01-c9msa.mongodb.net:27017,list-app-shard-00-02-c9msa.mongodb.net:27017/test?ssl=true&replicaSet=list-app-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true } );
+mongoose.connect(`mongodb://dbUser:${process.env.dbUserPassword}@list-app-shard-00-00-c9msa.mongodb.net:27017,list-app-shard-00-01-c9msa.mongodb.net:27017,list-app-shard-00-02-c9msa.mongodb.net:27017/test?ssl=true&replicaSet=list-app-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true }, (err) => {
+  if(err) console.log(err);
+  else console.log("Connected to the database!");
+});
 const bodyParser = require("body-parser");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
